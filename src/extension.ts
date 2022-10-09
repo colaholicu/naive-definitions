820d4825ea610d792483165d6277a48921190f0d
Iosif M <colaholicu@users.noreply.github.com>
2022-10-09 15:56:33 +0200

    + added support to find the 1st occurrence of a string within a workspace's files
    For testing purposes:
    * used hardcoded string
    * used hardcoded file types & path

--- src/extension.ts
+++ src/extension.ts
@@ -13,33 +13,71 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('naive-definitions.goToDefinition', () => {
+	let disposable = vscode.commands.registerCommand('naive-definitions.goToDefinition', async () => {
 		// The code you place here will be executed every time your command is executed
-		
+
 		// Get the active text editor
 		const editor = vscode.window.activeTextEditor;
 
 		if (editor) {
-			const document = editor.document;
-			const selection = editor.selection;
-
 			// Get the word within the selection
-			const selectedText = document.getText(selection);
+			// const selectedText = document.getText(editor.selection);
+			// for testing purposes -> find the 1st occurrence of the the getConfig() function in the opengrok workspace
+			const selectedText = "function getConfig()";
 			if (selectedText.length === 0) {
 				vscode.window.showInformationMessage("No text selected.");
 				return;
 			}
-			
-			// get the 1st occurrence within the current file
-			const documentText = document.getText();
-			const indexOfSelectedText = documentText.indexOf(selectedText);
-			const position = editor.document.positionAt(indexOfSelectedText);
 
-			// move cursor & reveal line
-			editor.selection = new vscode.Selection(position, position);
-			vscode.commands.executeCommand("revealLine", {
-				lineNumber: position.line
-			});			
+			// only work with workspaces
+			const workspaceFolders = vscode.workspace.workspaceFolders;
+			if (workspaceFolders === undefined) {
+				return;
+			}
+
+			// get the 1st occurrence within the current file
+			const documentText = editor.document.getText();
+			const indexOfSelectedText = documentText.indexOf(selectedText);
+			if (indexOfSelectedText !== -1) {
+				const position = editor.document.positionAt(indexOfSelectedText);
+				// move cursor & reveal line
+				editor.selection = new vscode.Selection(position, position);
+				vscode.commands.executeCommand("revealLine", {
+					lineNumber: position.line
+				});
+				return;
+			}
+
+			// find all .ts documents in src/ folders
+			const documents = await vscode.workspace.findFiles('**/src/**/*.ts');
+			if (!documents) {
+				return;
+			}
+
+			// fire read job for each document
+			let foundDefinition = false;
+			documents.forEach(async uri => {
+				const fileContents = await vscode.workspace.fs.readFile(uri);
+				const documentText = fileContents?.toString();
+				if (documentText.length && !foundDefinition) {
+					const indexOfSelectedText = documentText.indexOf(selectedText);
+					if (indexOfSelectedText === -1) {
+						return;
+					}
+
+					// found our definition => open and show the document
+					foundDefinition = true;
+					const document = await vscode.workspace.openTextDocument(uri);
+					await vscode.window.showTextDocument(document, { preview: false });
+
+					// focus at the line & column
+					const position = editor.document.positionAt(indexOfSelectedText);
+					editor.selection = new vscode.Selection(position, position);
+					vscode.commands.executeCommand("revealLine", {
+						lineNumber: position.line
+					});
+				}
+			});
 		}
 	});
 
@@ -47,4 +85,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
