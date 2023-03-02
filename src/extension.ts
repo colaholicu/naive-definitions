dee0789a098a3bbd9d2652190b57b88b0f3aa8f5
Iosif M <colaholicu@users.noreply.github.com>
2023-03-02 19:40:31 +0100

    + added proto basic scrubber

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { json } from 'stream/consumers';
+import { TextEncoder } from 'util';
 import * as vscode from 'vscode';
+
+type Definition = {
+	symbol: string;
+	file: string;
+	location: number;
+};
 
 enum SearchStatus {
 	setup,
@@ -214,10 +222,150 @@ class Searcher {
 	}
 }
 
+enum ScrubStatus {
+	setup,
+	idle,
+	scrubbing,
+	done,
+	complete,
+}
+
+class Scrubber {
+	selectedText: string = "";
+	potentialDefinition: string = "";
+	scrubFiles: vscode.Uri[] = [];
+	scrubStatus = ScrubStatus.setup;
+	definitionsMap = new Map<string, Definition[]>();
+	scrubbedFiles = new Map<string, string[]>();
+
+	definitions:string[] = vscode.workspace.getConfiguration("naive-definitions").definitions;
+	fileTypes: string = vscode.workspace.getConfiguration("naive-definitions").fileTypes;
+	generalMatcher: string = vscode.workspace.getConfiguration("naive-definitions").generalMatcher;
+	triedDefinitions: string[] = [];
+	filesScrubbed = 0;
+
+	setStatus(status: ScrubStatus) {
+		this.scrubStatus = status;
+
+		this.scrub();
+	}
+
+	async scrub() {
+		switch (this.scrubStatus) {
+			case ScrubStatus.setup:
+				await this.setupFileFilter();
+				break;
+
+			case ScrubStatus.idle:
+				this.scrubWorkspace();
+				break;
+
+			case ScrubStatus.scrubbing:
+				break;
+
+			case ScrubStatus.done:
+				let uint8Array = new TextEncoder().encode("iosif");
+				vscode.workspace.fs.writeFile(vscode.Uri.joinPath(vscode.workspace.workspaceFolders![0].uri, "./.naive/def"), uint8Array);
+				break;
+		}
+	}
+
+	scrubWorkspace() {
+		// tried all definitions -> we're done
+		if (this.triedDefinitions.length === this.definitions.length) {
+			this.setStatus(ScrubStatus.done);
+		}
+
+		// try one more definition
+		for (let definition of this.definitions) {
+			let found = false;
+			for (let triedDefinition of this.triedDefinitions) {
+				if (triedDefinition === definition) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				this.triedDefinitions.push(definition);
+				break;
+			}
+		}
+
+		let definition = this.triedDefinitions.at(this.triedDefinitions.length - 1);
+		if (definition) {
+			// if this current definition is a regex (maybe different from the general one) -> has priority
+			const regexToken = "${regex}=";
+			const definitionToken = "${DEFINITION}";
+			const selectedTextToken = "${SELECTED_TEXT}";
+			if (definition.indexOf(regexToken) >= 0) {
+				this.potentialDefinition = definition.replace(regexToken, "");
+
+				if (definition.indexOf(definitionToken) !== -1) {
+					vscode.window.showInformationMessage("Invalid token ${DEFINITION} found in definition " + definition + ". Ignoring!");
+					this.setStatus(ScrubStatus.idle);
+				}
+
+				// the selected text token isn't present -> just append it
+				if (this.potentialDefinition.indexOf(selectedTextToken) === -1) {
+					this.potentialDefinition += this.selectedText;
+				}
+				// the selected text token is present -> replace it
+				else {
+					this.potentialDefinition = this.potentialDefinition.replace(selectedTextToken, this.selectedText);
+				}
+			}
+			// current definition wasn't a regex, but we have a regex expression that's a general matching rule -> replace tokens
+			else if (this.generalMatcher.length > 0) {
+				this.potentialDefinition = this.generalMatcher;
+				this.potentialDefinition = this.potentialDefinition.replace(definitionToken, definition);
+				this.potentialDefinition = this.potentialDefinition.replace(selectedTextToken, this.selectedText);
+			}
+			// no general matching rule, just append selected text to the definition
+			else {
+				this.potentialDefinition = definition;
+			}
+		}
+
+		
+		this.scrubFiles.forEach(async uri => {			
+			const fileContents = await vscode.workspace.fs.readFile(uri);
+			if (fileContents) {
+				const regexp = new RegExp("(" + this.potentialDefinition + ")(\\W*)(\\w*)", "g");
+                const matches = fileContents.toString().matchAll(regexp);
+                for (const match of matches) {
+					const symbol = match[3].toString();
+					if (!this.definitionsMap.has(symbol)) {
+						this.definitionsMap.set(symbol, []);
+
+					}
+					
+					this.definitionsMap.get(symbol)!.push({ file : uri.toString(), location : match.index!, symbol: ''});
+                }
+                this.filesScrubbed++;
+				// tried all files --> try another definition
+				if (this.filesScrubbed === this.scrubFiles.length) {
+					this.setStatus(ScrubStatus.done);
+					return;
+				}
+			}
+		});
+
+		this.setStatus(ScrubStatus.scrubbing);
+	}
+
+	async setupFileFilter() {
+		this.scrubFiles = await vscode.workspace.findFiles(this.fileTypes);
+		if (!this.scrubFiles) {
+			return;
+		}
+
+		this.setStatus(ScrubStatus.idle);
+	}
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
+export function activate(context: vscode.ExtensionContext) {	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -247,6 +395,9 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	let scrubber = new Scrubber();
+	scrubber.scrub();
 }
 
 // this method is called when your extension is deactivated
